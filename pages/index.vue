5de0898f3e51fc8b10a45fe999cb4350c3d6e67e
ADITIO PANGESTU <aditiopangestu@sehatqs-MacBook-Air.local>
2023-07-05 15:46:12 +0700

    init prettier and eslint setup

--- pages/index.vue
+++ pages/index.vue
@@ -21,7 +21,7 @@
           </button>
         </Tab>
       </TabList>
-  
+
       <TabPanels class="mt-2">
         <TabPanel
           v-for="(posts, idx) in Object.values(categories)"
@@ -40,7 +40,7 @@
               <h3 class="text-sm font-medium leading-5">
                 {{ post.title }}
               </h3>
-  
+
               <ul
                 class="mt-1 flex space-x-1 text-xs font-normal leading-4 text-gray-500"
               >
@@ -50,7 +50,7 @@
                 <li>&middot;</li>
                 <li>{{ post.shareCount }} shares</li>
               </ul>
-  
+
               <a
                 href="#"
                 :class="[
@@ -65,59 +65,59 @@
     </TabGroup>
   </div>
 </template>
-  
-  <script setup>
-  import { ref } from 'vue'
-  import { TabGroup, TabList, Tab, TabPanels, TabPanel } from '@headlessui/vue'
-  
-  const categories = ref({
-    Recent: [
-      {
-        id: 1,
-        title: 'Does drinking coffee make you smarter?',
-        date: '5h ago',
-        commentCount: 5,
-        shareCount: 2,
-      },
-      {
-        id: 2,
-        title: "So you've bought coffee... now what?",
-        date: '2h ago',
-        commentCount: 3,
-        shareCount: 2,
-      },
-    ],
-    Popular: [
-      {
-        id: 1,
-        title: 'Is tech making coffee better or worse?',
-        date: 'Jan 7',
-        commentCount: 29,
-        shareCount: 16,
-      },
-      {
-        id: 2,
-        title: 'The most innovative things happening in coffee',
-        date: 'Mar 19',
-        commentCount: 24,
-        shareCount: 12,
-      },
-    ],
-    Trending: [
-      {
-        id: 1,
-        title: 'Ask Me Anything: 10 answers to your questions about coffee',
-        date: '2d ago',
-        commentCount: 9,
-        shareCount: 5,
-      },
-      {
-        id: 2,
-        title: "The worst advice we've ever heard about coffee",
-        date: '4d ago',
-        commentCount: 1,
-        shareCount: 2,
-      },
-    ],
-  })
-  </script>
+
+<script setup>
+import { ref } from "vue";
+import { TabGroup, TabList, Tab, TabPanels, TabPanel } from "@headlessui/vue";
+
+const categories = ref({
+  Recent: [
+    {
+      id: 1,
+      title: "Does drinking coffee make you smarter?",
+      date: "5h ago",
+      commentCount: 5,
+      shareCount: 2,
+    },
+    {
+      id: 2,
+      title: "So you've bought coffee... now what?",
+      date: "2h ago",
+      commentCount: 3,
+      shareCount: 2,
+    },
+  ],
+  Popular: [
+    {
+      id: 1,
+      title: "Is tech making coffee better or worse?",
+      date: "Jan 7",
+      commentCount: 29,
+      shareCount: 16,
+    },
+    {
+      id: 2,
+      title: "The most innovative things happening in coffee",
+      date: "Mar 19",
+      commentCount: 24,
+      shareCount: 12,
+    },
+  ],
+  Trending: [
+    {
+      id: 1,
+      title: "Ask Me Anything: 10 answers to your questions about coffee",
+      date: "2d ago",
+      commentCount: 9,
+      shareCount: 5,
+    },
+    {
+      id: 2,
+      title: "The worst advice we've ever heard about coffee",
+      date: "4d ago",
+      commentCount: 1,
+      shareCount: 2,
+    },
+  ],
+});
+</script>
